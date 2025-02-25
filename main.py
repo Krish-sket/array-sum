0d4897a73cc0589ee8c08bf87d3f833599e7f361
Krish-sket <theworldofgamingksmvd@gmail.com>
2025-02-25 20:27:20 +0530

    edit

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def sum(a):
     if length==1:
         return a[0]
     return a[0]+ sum(a[1:])
-a=[1,5,3,77,5,8]
+user_input = input("Enter numbers separated by space: ")
+a = list(map(int, user_input.split()))
 print('The sum of array a is:',sum(a))
